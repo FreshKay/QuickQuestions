6c7e5b1715aa14d03baffdf83503b667b5290bb5
FreshKay <mikolaj.jon@gmail.com>
2019-01-21 15:00:47 +0100

    Add quiz text controller

--- QuickQuestions/Controllers/QuizController.cs
+++ QuickQuestions/Controllers/QuizController.cs
@@ -57,5 +57,52 @@ namespace QuickQuestions.Controllers
 
             return View();
         }
-    }
+
+        [HttpGet]
+        public ActionResult QuizTest()
+        {
+            QuizViewModel quizSelected = Session["SelectedQuiz"] as QuizViewModel;
+            IQueryable<QuestionViewModel> questions = null;
+
+            if (quizSelected != null)
+            {
+                questions = db.Questions.Where(q => q.Quiz.QuizID == quizSelected.QuizID)
+                   .Select(q => new QuestionViewModel
+                   {
+                       QuestionID = q.QuestionID,
+                       QuestionText = q.QuestionText,
+                       Choices = q.Choices.Select(c => new ChoiceViewModel
+                       {
+                           ChoiceID = c.ChoiceID,
+                           ChoiceText = c.ChoiceText
+                       }).ToList()
+
+                   }).AsQueryable();
+
+
+            }
+
+            return View(questions);
+        }
+
+        [HttpPost]
+        public ActionResult QuizTest(List<QuizAnswersViewModel> resultQuiz)
+        {
+            List<QuizAnswersViewModel> finalResultQuiz = new List<ViewModels.QuizAnswersViewModel>();
+
+            foreach (QuizAnswersViewModel answser in resultQuiz)
+            {
+                QuizAnswersViewModel result = db.Answers.Where(a => a.QuestionID == answser.QuestionID).Select(a => new QuizAnswersViewModel
+                {
+                    QuestionID = a.QuestionID.Value,
+                    AnswerQ = a.AnswerText,
+                    isCorrect = (answser.AnswerQ.ToLower().Equals(a.AnswerText.ToLower()))
+
+                }).FirstOrDefault();
+
+                finalResultQuiz.Add(result);
+            }
+
+            return Json(new { result = finalResultQuiz }, JsonRequestBehavior.AllowGet);
+        }
 }
